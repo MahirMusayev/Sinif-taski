18b947c01e418eef9c9e16e5792880f3e1b13929
Mahir Musayev <146106439+musayevmahir68@users.noreply.github.com>
2024-04-15 23:19:50 +0400

    first commit

---
+++ Sinif taski/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Sinif_taski
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test test = new Test();
+            //test.Arr[0] = 1;
+            //for (int i = 0; i < test.Arr.Length; i++)
+            //{
+            //    Console.WriteLine(test.Arr[i]);
+            //}
+            //foreach (var item in typeof(Test).GetProperties())
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+            do
+            {
+                Console.WriteLine("Please enter Name:");
+                string name = Console.ReadLine();
+                try
+                {
+                    Person person = new Person(name);
+                    Console.WriteLine(person.Name);
+                    break;
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } while (true);
+
+        }
+        public static Person CreatePerson()
+        {
+            Console.WriteLine("Please enter Name:");
+            string name = Console.ReadLine();
+            try
+            {
+                Person person = new Person(name);
+                return person;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return CreatePerson();
+            }
+        }
+
+
+    }
+}
---
+++ Sinif taski/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinif_taski
+{
+    internal class Test
+    {
+
+        public int[] Arr { get; set; }
+        public int this[int i]
+        {
+            get { return Arr[i]; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("deyer 0 - dan kicik ola bilmez");
+                if (i > Arr.Length)
+                    throw new IndexOutOfRangeException();
+            }
+        }
+        public Test()
+        {
+            Arr = new int[10];
+        }
+    }
+}
